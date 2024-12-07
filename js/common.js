76096f06cc130e1ac6259cc2811ad5049494de03
Cesar Reyes <cereandel@gmail.com>
2024-12-07 01:20:06 -0400

    matenme

--- js/common.js
+++ js/common.js
@@ -48,29 +48,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if (currentPath.endsWith("index.html")) {
       innerDiv.innerHTML = `
-        <h3 class="masthead-brand">Batalla Naval</h3>
+        <h3 class="masthead-brand">Batalla Naval <svg width="3vw" height="3vw" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 1H5V3.89181L7.99999 2.89182L11 3.89182V1Z" fill="#000000"></path> <path d="M8 5L2 7L3 13H1V15H3.19258L5.5 14.077L8 15.077L10.5 14.077L12.8074 15H15V13H13L14 7L8 5Z" fill="#000000"></path> </g></svg></h3>
         <nav class="nav nav-masthead justify-content-center">
           <a class="nav-link active" href="index.html">Home</a>
           <a class="nav-link" href="html/instrucciones.html">Instrucciones</a>
-          <a class="nav-link" href="#">Leaderboard</a>
         </nav>
       `;
     } else if (currentPath.endsWith("instrucciones.html")) {
       innerDiv.innerHTML = `
-        <h3 class="masthead-brand">Batalla Naval</h3>
+        <h3 class="masthead-brand">Batalla Naval <svg width="3vw" height="3vw" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 1H5V3.89181L7.99999 2.89182L11 3.89182V1Z" fill="#000000"></path> <path d="M8 5L2 7L3 13H1V15H3.19258L5.5 14.077L8 15.077L10.5 14.077L12.8074 15H15V13H13L14 7L8 5Z" fill="#000000"></path> </g></svg></h3>
         <nav class="nav nav-masthead justify-content-center">
           <a class="nav-link" href="../index.html">Home</a>
           <a class="nav-link active" href="instrucciones.html">Instrucciones</a>
-          <a class="nav-link" href="#">Leaderboard</a>
         </nav>
       `;
     } else if (currentPath.endsWith("juego.html")) {
       innerDiv.innerHTML = `
-        <h3 class="masthead-brand">Batalla Naval</h3>
+        <h3 class="masthead-brand">Batalla Naval<svg width="3vw" height="3vw" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M11 1H5V3.89181L7.99999 2.89182L11 3.89182V1Z" fill="#000000"></path> <path d="M8 5L2 7L3 13H1V15H3.19258L5.5 14.077L8 15.077L10.5 14.077L12.8074 15H15V13H13L14 7L8 5Z" fill="#000000"></path> </g></svg></h3>
         <nav class="nav nav-masthead justify-content-center">
           <a class="nav-link" href="../index.html">Home</a>
           <a class="nav-link" href="../html/instrucciones.html">Instrucciones</a>
-          <a class="nav-link" href="#">Leaderboard</a>
         </nav>
       `;
     }
